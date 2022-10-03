0f342774f2678ef829e2bc429851caf04c472e05
currocam <campuzanocurro@gmail.com>
2022-10-03 18:52:01 +0200

    test: print_line

--- src/format_bed_test.py
+++ src/format_bed_test.py
@@ -2,8 +2,10 @@
 # all files that start with test_*.py and run all functions with
 # names that start with test_
 
+from sys import stdout
+import sys
 import pytest
-from bed import BedLine, parse_bed
+from bed import BedLine, parse_bed, print_line
 
 
 def test_it_works_with_general_bed_line():
@@ -23,3 +25,10 @@ def test_it_raises_exception_when_less_columns():
     line_with_less_columns = "chr7    127471196                  foo"
     with pytest.raises(ValueError):
         parse_bed(line_with_less_columns)
+
+
+def test_it_prints_strict_BedLine(capsys):
+    bed = BedLine(chrom='chr1', chrom_start=20100, chrom_end=20101, name='foo')
+    print_line(bed, sys.stdout)
+    captured = capsys.readouterr()
+    assert captured.out == 'chr1\t20100\t20101\tfoo\n'
