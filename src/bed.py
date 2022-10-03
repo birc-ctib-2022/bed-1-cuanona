73f6f7cccf53e3e0684eab97aedf6825327d9ea9
currocam <campuzanocurro@gmail.com>
2022-10-03 15:01:03 +0200

    tests: parse_line

--- src/bed.py
+++ src/bed.py
@@ -28,7 +28,6 @@ def parse_line(line: str) -> BedLine:
     """
     chrom, start, end, name = line.split()  # split on any white-space
     bed_line = BedLine(chrom, int(start), int(end), name)
-    assert bed_line.chrom_start + 1 == bed_line.chrom_end
     return bed_line
 
 
